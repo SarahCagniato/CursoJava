1145774beaef145e1ac4389aabc27fd9cf9a0489
Sarah Moura Cagniato <123985537+SarahCagniato@users.noreply.github.com>
2023-05-22 17:20:17 -0300

    Update SarahCagniato_Exercicio5.java

--- SarahCagniato_SegundaLista/SarahCagniato_Exercicio5.java
+++ SarahCagniato_SegundaLista/SarahCagniato_Exercicio5.java
@@ -9,12 +9,13 @@ import java.util.Scanner;
 public class exercicio5 {
   public static void main(String[] args) {
     try (Scanner input = new Scanner(System.in)) {
+        //pede um número
       System.out.print("Digite um número: ");
       int numero = input.nextInt();
-
+        // calcula o antecessor e o sucessor
       int antecessor = numero - 1;
       int sucessor = numero + 1;
-
+        //mostra os resultados
       System.out.println("O antecessor de " + numero + " é: " + antecessor);
       System.out.println("O número digitado é: " + numero);
       System.out.println("O sucessor de " + numero + " é: " + sucessor);
