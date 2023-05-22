7c6bfe859b11cbbea9c2f2aa35ab54c788a76c40
Sarah Moura Cagniato <123985537+SarahCagniato@users.noreply.github.com>
2023-05-22 17:10:24 -0300

    Update SarahCagniato_Exercicio1.java

--- SarahCagnaito_PrimeiraLista/SarahCagniato_Exercicio1.java
+++ SarahCagnaito_PrimeiraLista/SarahCagniato_Exercicio1.java
@@ -9,20 +9,21 @@ import java.util.Scanner;
 public class exercicio1 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
+            //pede o primeiro número 
             System.out.println("Digite o primeiro número:");
             int n1 = scanner.nextInt();
-
+            //pede o segundo número 
             System.out.println("Digite o segundo número:");
             int n2 = scanner.nextInt();
-
+            //pede o terceiro número 
             System.out.println("Digite o terceiro número:");
             int n3 = scanner.nextInt();
-
+            //pede o quarto número 
             System.out.println("Digite o quarto número:");
             int n4 = scanner.nextInt();
-
+            //calcula o resultado
             int resultado = n1 + n2 + n3 + n4;
-
+            //mostra a soma
             System.out.println("A soma dos números é: " + resultado);
         }
     }
