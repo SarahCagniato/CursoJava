d850cb2121267ce97944c1b9a60ba99718bf1d06
Sarah Moura Cagniato <123985537+SarahCagniato@users.noreply.github.com>
2023-05-22 17:19:20 -0300

    Update SarahCagniato_Exercicio4.java

--- SarahCagniato_SegundaLista/SarahCagniato_Exercicio4.java
+++ SarahCagniato_SegundaLista/SarahCagniato_Exercicio4.java
@@ -9,16 +9,17 @@ import java.util.Scanner;
 public class exercicio4 {
   public static void main(String[] args) {
     try (Scanner input = new Scanner(System.in)) {
+        //pede o primeiro valor
       System.out.print("Digite o valor A: ");
       int valorA = input.nextInt();
-
+        //pede o seguundo valor
       System.out.print("Digite o valor B: ");
       int valorB = input.nextInt();
-
+        //troca os valores
       int temp = valorA;
       valorA = valorB;
       valorB = temp;
-
+    //mostra a troca
       System.out.println("Valor A após a troca: " + valorA);
       System.out.println("Valor B após a troca: " + valorB);
     }
