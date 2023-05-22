ca95907b7061bb97a263b5187f0c6ef39a937385
Sarah Moura Cagniato <123985537+SarahCagniato@users.noreply.github.com>
2023-05-22 17:17:57 -0300

    Update SarahCagniato_Exercicio3.java

--- SarahCagniato_SegundaLista/SarahCagniato_Exercicio3.java
+++ SarahCagniato_SegundaLista/SarahCagniato_Exercicio3.java
@@ -9,14 +9,15 @@ import java.util.Scanner;
 public class exercicio3 {
   public static void main(String[] args) {
     try (Scanner input = new Scanner(System.in)) {
+        //pede a base do triangulo
       System.out.print("Digite a base do triângulo: ");
       double base = input.nextDouble();
-
+        //pede a altura do triangulo
       System.out.print("Digite a altura do triângulo: ");
       double altura = input.nextDouble();
-
+        //calcula a area
       double area = (base * altura) / 2;
-
+        //mostra o resultado
       System.out.println("A área do triângulo é: " + area);
     }
   }
