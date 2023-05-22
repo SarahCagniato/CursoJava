afbe8499d73ac140ed7f01ad86233cc3e8ee56fa
Sarah Moura Cagniato <123985537+SarahCagniato@users.noreply.github.com>
2023-05-22 17:12:08 -0300

    Update SarahCagniato_Exercicio1.java

--- SarahCagniato_SegundaLista/SarahCagniato_Exercicio1.java
+++ SarahCagniato_SegundaLista/SarahCagniato_Exercicio1.java
@@ -9,12 +9,13 @@ import java.util.Scanner;
 public class exercicio1 {
   public static void main(String[] args) {
     try (Scanner input = new Scanner(System.in)) {
+        //pede um número
       System.out.print("Digite um valor: ");
       double valor = input.nextDouble();
-
+            //calcula o desconto
       double desconto = valor * 0.15;
       double valorComDesconto = valor - desconto;
-
+            //mostra o resultado
       System.out.println("Valor original: R$" + valor);
       System.out.println("Valor com desconto: R$" + valorComDesconto);
     }
