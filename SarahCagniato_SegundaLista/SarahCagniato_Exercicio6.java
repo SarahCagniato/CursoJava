3d67597695f8fd53790b79af6fbac2fcb11fa3d3
Sarah Moura Cagniato <123985537+SarahCagniato@users.noreply.github.com>
2023-05-22 17:26:43 -0300

    Update SarahCagniato_Exercicio6.java

--- SarahCagniato_SegundaLista/SarahCagniato_Exercicio6.java
+++ SarahCagniato_SegundaLista/SarahCagniato_Exercicio6.java
@@ -10,21 +10,22 @@ import java.util.Scanner;
 public class exercicio6 {
   public static void main(String[] args) {
     try (Scanner input = new Scanner(System.in)) {
+        // preço dos picolés
       double precoKiwi = 3.20;
       double precoLaranja = 2.39;
       double precoRoma = 7.99;
-
+        //pede a quantidade de cada sabor de picole
       System.out.print("Quantidade de picolés de Kiwi a ser comprado: ");
       int quantidadeKiwi = input.nextInt();
-
+       
       System.out.print("Quantidade de picolés de Laranja a ser comprado: ");
       int quantidadeLaranja = input.nextInt();
 
       System.out.print("Quantidade de picolés de Romã a ser comprado: ");
       int quantidadeRoma = input.nextInt();
-
+        //calcaula o valor a cobrar
       double total = (quantidadeKiwi * precoKiwi) + (quantidadeLaranja * precoLaranja) + (quantidadeRoma * precoRoma);
-
+        // mostra o valor
       System.out.println("O valor total da compra é: R$ " + total);
     }
   }
