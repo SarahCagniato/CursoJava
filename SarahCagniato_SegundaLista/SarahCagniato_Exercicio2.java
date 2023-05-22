55982f2b61d17fcc6d641e53b6ca9f35f67fc014
Sarah Moura Cagniato <123985537+SarahCagniato@users.noreply.github.com>
2023-05-22 17:16:38 -0300

    Update SarahCagniato_Exercicio2.java

--- SarahCagniato_SegundaLista/SarahCagniato_Exercicio2.java
+++ SarahCagniato_SegundaLista/SarahCagniato_Exercicio2.java
@@ -9,15 +9,16 @@ import java.util.Scanner;
 public class exercicio2 {
   public static void main(String[] args) {
     try (Scanner input = new Scanner(System.in)) {
+       //pede o primeiro número 
       System.out.print("Digite o primeiro número: ");
       int numero1 = input.nextInt();
-
+       //pede o segundo número 
       System.out.print("Digite o segundo número: ");
       int numero2 = input.nextInt();
-
+       //pede o terceiro número 
       System.out.print("Digite o terceiro número: ");
       int numero3 = input.nextInt();
-
+       // calcula o maior numero
       int maior = numero1;
 
       if (numero2 > maior) {
@@ -27,7 +28,7 @@ public class exercicio2 {
       if (numero3 > maior) {
         maior = numero3;
       }
-
+            //mostra o resultado
       System.out.println("O maior número é: " + maior);
     }
   }
